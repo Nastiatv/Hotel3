1a0ad007697ca649208f934f6edef399e5fd6888
Nastiatv <57249641+Nastiatv@users.noreply.github.com>
2020-02-05 10:58:06 +0300

    +

--- Hotel new 2/hotel/src/main/java/com/runa/hotel/controllers/GuestController.java
+++ Hotel new 2/hotel/src/main/java/com/runa/hotel/controllers/GuestController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.runa.hotel.api.dto.GuestDto;
 import com.runa.hotel.api.service.IGuestService;
