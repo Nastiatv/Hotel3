b2b6eb09376e384b2fb66c0c21aae36c6ddc1522
Nastiatv <57249641+Nastiatv@users.noreply.github.com>
2020-02-28 20:35:49 +0300

    +

--- Hotel new 2/hotel/src/main/java/com/runa/hotel/api/dao/IAGenericDao.java
+++ Hotel new 2/hotel/src/main/java/com/runa/hotel/api/dao/IAGenericDao.java
@@ -2,11 +2,9 @@ package com.runa.hotel.api.dao;
 
 import java.util.List;
 
-import org.springframework.stereotype.Repository;
-
 import com.runa.hotel.entities.AEntity;
 
-@Repository
+//@Repository
 public interface IAGenericDao<T extends AEntity>  {
 	
 	Class<T> getGenericClass();
